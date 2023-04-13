e32616dba74f8aa3d0553d758e95d99b2a0fe204
Pau Martinez <paumartinez@pop-os.localdomain>
2023-04-13 14:46:19 +0200

    changes

--- FullSport_Backend/routes/web.php
+++ FullSport_Backend/routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('getIndicators', function()
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://v3.football.api-sports.io/{endpoint}',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'x-rapidapi-key: XxXxXxXxXxXxXxXxXxXxXxXx',
+        'x-rapidapi-host: v3.football.api-sports.io'
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+});
