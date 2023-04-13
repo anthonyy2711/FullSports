1bac1c7fff8beec20e7a17a01d0c746fe9f87599
anthonyy2711 <amreyesnarvaez@gmail.com>
2023-04-13 13:14:28 +0200

    components

--- FullSport_Frontend/src/app/app.module.ts
+++ FullSport_Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
