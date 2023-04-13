9057a0ad6bb4a737603456ad51713a441de7052d
anthonyy2711 <amreyesnarvaez@gmail.com>
2023-04-13 13:18:52 +0200

    componentes

--- FullSport_Frontend/src/app/app.module.ts
+++ FullSport_Frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PruebaComponent
+    PruebaComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
