96c0ac6d8b059f6048d256ab43d1aab7dbfc270b
ruyouhuye <ruyouhuye@gmail.com>
2023-05-19 14:48:56 +0200

    addnews done

--- FullSport_Frontend/src/app/components/newsadd/newsadd.component.ts
+++ FullSport_Frontend/src/app/components/newsadd/newsadd.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-newsadd',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./newsadd.component.css']
 })
 export class NewsaddComponent {
+  file:any;
+  constructor(public formBuilder: FormBuilder,private router: Router,private ngZone: NgZone,private newsService: NewsService ) {}
 
+  postForm=new FormGroup({
+    new_title:new FormControl('',[
+      Validators.required,
+      Validators.maxLength(20)
+    ]),
+    new_description:new FormControl('',[
+      Validators.required
+    ]),
+    new_img:new FormControl('',[
+      Validators.required
+    ]),
+  })
+
+  ngOnInit() { }
+
+  onSubmit(): any {
+
+    var new_title  = this.postForm.get('new_title')?.value;
+    var new_description  = this.postForm.get('new_description')?.value;
+    var image  = this.postForm.get('formFile')?.value;
+    var user_id = localStorage.getItem('user_id');
+    var username = localStorage.getItem('username');
+
+    const formData = new FormData();
+    formData.append('new_img', this.file);
+    formData.append('new_title',new_title!);
+    formData.append('new_description',new_description!);
+    formData.append('user_id',user_id!);
+    formData.append('author_name',username!);
+
+
+    //console.log(data)
+    this.newsService.AddNews(formData)
+    .subscribe(() => {
+
+        this.ngZone.run(() => this.router.navigate(['home']))
+      }, (err) => {
+
+    });
+  }
+  imageUpload(event:any){
+    console.log(event);
+    this.file = event.target.files[0];
+    console.log(this.file)
+  }
 }
--- FullSport_Frontend/src/app/services/news.service.ts
+++ FullSport_Frontend/src/app/services/news.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { News } from '../model/news';
-import { Observable, throwError } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  REST_API: string = 'http://127.0.0.1:8000';
+  REST_API: string = 'http://127.0.0.1:8000/api/';
 
   httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
   constructor(private httpClient: HttpClient) { }
@@ -19,6 +19,16 @@ export class NewsService {
 
   ShowNews(id:any): Observable<News> {
     return this.httpClient.get<News>(`http://127.0.0.1:8000/api/news/show/${id}`);
+  }
+
+  // Add news
+  AddNews(data:any): Observable<any> {
+
+    let API_URL = `${this.REST_API}news`;
+    return this.httpClient.post(API_URL, data,{ responseType: 'blob' })
+      .pipe(
+        catchError(this.handleError)
+      )
   }
 
   handleError(error: HttpErrorResponse) {
